dff38c469ea48197ed579283ab6cb79e1bcbf26e
anuruddha-thennakoon <anuruddha-thennakoon@gmail.com>
2017-05-11 11:06:03 +0530

    w3 css/w3.css added and a sample

--- app/controllers/register_drugs.controller.js
+++ app/controllers/register_drugs.controller.js
@@ -1,5 +1,27 @@
 //controller for register drugs
-pharmacyModuleApp.controller('registerDrugsController', function ($scope) {
+/*pharmacyModuleApp.controller('registerDrugsController', function ($scope) {
     $scope.message = 'This message is getting from register drugs controller';
-});
+});*/
 
+pharmacyModuleApp.controller('TooltipDemoCtrl', function ($scope, $sce) {
+  $scope.dynamicTooltip = 'Hello, World!';
+  $scope.dynamicTooltipText = 'dynamic';
+  $scope.htmlTooltip = $sce.trustAsHtml('I\'ve been made <b>bold</b>!');
+  $scope.placement = {
+    options: [
+      'top',
+      'top-left',
+      'top-right',
+      'bottom',
+      'bottom-left',
+      'bottom-right',
+      'left',
+      'left-top',
+      'left-bottom',
+      'right',
+      'right-top',
+      'right-bottom'
+    ],
+    selected: 'top'
+  };
+});
