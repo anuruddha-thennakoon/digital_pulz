5d221c00b540e613da1e5c490ab01a53fc56ae55
anuruddha-thennakoon <anuruddha.thennakoon@gmail.com>
2017-05-15 11:04:35 +0530

    New modification

---
+++ app/views/assets/css/custom.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'border': {
+    'borderStyle': 'solid',
+    'borderWidth': '1px',
+    'width': [{ 'unit': 'px', 'value': 250 }]
+  },
+  'select': {
+    'width': [{ 'unit': 'px', 'value': 400 }],
+    'height': [{ 'unit': 'px', 'value': 40 }]
+  },
+  'button': {
+    'borderRadius': '5px'
+  }
+});
