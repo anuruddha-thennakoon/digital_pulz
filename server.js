cb5ef5616424ae51daea5c2ec67f0b55ca086cef
Anuruddha Thennakoon <anuruddha.thennakoon@gmail.com>
2017-06-29 14:59:44 +0530

    new modifications in total project

--- server.js
+++ server.js
@@ -1,18 +1,7 @@
 'use strict';
 
 const express = require('express');
-const mongoose = require('mongoose');
-
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost:27017/digital_pulz', err => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-});
-
-const app = require('./app/services/all.services');
+const app = express();
 
 app.use(express.static(__dirname));
 
