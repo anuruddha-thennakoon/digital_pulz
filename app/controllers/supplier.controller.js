28e7a92e9117de30a24b930cb36da4d35bbf857b
CheeBhagyani <cheebhagyani1994@gmail.com>
2017-07-01 08:09:48 +0530

    supplier modifications

---
+++ app/controllers/supplier.controller.js
@@ -0,0 +1,22 @@
+//supplier controller
+pharmacyModuleApp.controller('suppplierController', function ($scope, $http) {
+
+    //function for get all users in db
+    // var getSuppliers = function () {
+    //     $http.get('/suppliers').then(function (response) {
+    //         $scope.supplier = response.data;
+    //     });
+    // };
+
+    // getSuppliers();
+
+    // //function for add new user
+    // $scope.addSupplier = function () {
+    //     $http.post('/suppliers', $scope.newSupplier).then(function (response) {
+    //         console.log(response.data);
+    //         $scope.newSupplier = "";
+    //     });
+    // };
+    
+
+});
