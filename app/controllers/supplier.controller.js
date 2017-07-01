9d597b36e42fe2086a80a87b36da541e7a39d880
CheeBhagyani <cheebhagyani1994@gmail.com>
2017-07-01 15:29:30 +0530

    supplier service

--- app/controllers/supplier.controller.js
+++ app/controllers/supplier.controller.js
@@ -3,37 +3,16 @@ pharmacyModuleApp.controller('suppplierController', function ($scope, $http) {
 
     //function for get all users in db
     var getSuppliers = function () {
-
-        var req = {
-            method: 'GET',
-            url: 'http://localhost:8000/api/supplier/',
-            header: {
-                token : undefined
-            }
-            
-        }
-
-
-        $http(req).then(function (response) {
+        $http.get('http://localhost:8000/api/supplier').then(function (response) {
             $scope.supplier = response.data;
         });
     };
-    // var getSuppliers = function () {
-    //     $http.get('/suppliers').then(function (response) {
-    //         $scope.supplier = response.data;
-    //     });
-    // };
 
     getSuppliers();
 
     //function for add new user
     $scope.addSupplier = function () {
-        var req = {
-            method: 'POST',
-            url: 'http://localhost:8000/api/supplier/'
-            
-        }
-        $http(req, $scope.newSupplier).then(function (response) {
+        $http.post('http://localhost:8000/api/supplier', $scope.newSupplier).then(function (response) {
             console.log(response.data);
             $scope.newSupplier = "";
         });
