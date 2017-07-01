b536434f7544989cfce30fa9db21df0d03cba0c8
CheeBhagyani <cheebhagyani1994@gmail.com>
2017-07-01 19:40:10 +0530

    drug anuruddha

--- app/controllers/add_drugs.controller.js
+++ app/controllers/add_drugs.controller.js
@@ -1,4 +1,19 @@
 //controller for register drugs
 pharmacyModuleApp.controller('addDrugsController', function ($scope) {
-    $scope.message = 'This message is getting from register drugs controller';
+   var getDrugs = function () {
+        $http.get('http://localhost:8000/api/drugs').then(function (response) {
+            $scope.drug = response.data;
+            console.log( response.data);
+        });
+    };
+
+    getDrugs();
+
+    //function for add new user
+    $scope.addDrugs = function () {
+        $http.post('http://localhost:8000/api/drugs', $scope.newDrug).then(function (response) {
+            console.log(response.data);
+            $scope.newDrug = "";
+        });
+    };
 });
